7f5105332402889dec79a12dedb91f2d610c2c71
joshua chivile <joshuachivile@gmail.com>
2024-01-11 17:41:58 +0300

    working with apis in python

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,17 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    # the endpoint 
+    URL = "https://data.cityofchicago.org/resource/ydr8-5enu.json"
 
     response = requests.get(URL)
     return response.content
+  
+  def program_agencies(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs_list = []
+      programs = json.loads(self.get_programs())
+      return json.dumps(programs, indent=3, sort_keys=True)
 
-programs = GetPrograms().get_programs()
+programs = GetPrograms().program_agencies()
 print(programs)
